f2338667181a47bd74191c9efa23c8eec4adb34d
olawale <jao.olaniyi@gmail.com>
2022-09-04 12:33:49 +0100

    worked on the javascript and also refactor my code to DRY

--- script.js
+++ script.js
@@ -1,1 +1,22 @@
 'use strict';
+let openModal = document.querySelectorAll('.open-Modal')
+let modal = document.querySelector('.modal')
+let btnClose = document.querySelector('.btn-close')
+let overlay =document.querySelector('.overlay')
+// creating a function for the displayModal
+let displayModal = function() {
+    modal.classList.remove('hidden')
+    overlay.classList.remove('hidden')
+}
+// looping through the array to get each button
+for(let i = 0; i < openModal.length; i++){
+openModal[i].addEventListener('click', displayModal )
+}
+// create a function for the hideModal which is reusable
+let hideModal = function(){
+    modal.classList.add('hidden')
+    overlay.classList.add('hidden')
+}
+
+btnClose.addEventListener('click', hideModal)
+overlay.addEventListener('click', hideModal)
